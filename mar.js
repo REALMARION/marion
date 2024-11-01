1e7a0b62869c751ca2c0667e130be6386e0299e3
MARIONHake <97278782+marionhake@users.noreply.github.com>
2024-11-01 15:01:07 +0200

    Update mar.js

--- mar.js
+++ mar.js
@@ -1,1 +1,17 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
 
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+
+if (isPrime(randomNumber)) {
+    
+} else {
+    
+}
